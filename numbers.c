4aec85b5c10bc1dc95f198af55d4ae731f025f59
kmmerc18 <integerninja@gmail.com>
2020-01-29 14:38:22 -0500

    numbers

---
+++ numbers.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+// Messing around wtih number systems
+
+void print_binary(int d) {
+	// finish on wednesday
+
+int main() {
+	int bits = 0b11111111;
+	printf("%d\n", bits);
+	
+	// 9 = 2^3 and 2^0: 1001
+	// 13 = 2^3, 2^2, 2^0: 1101
+	
+}
