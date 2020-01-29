956f445dcc775c27e9e0fd0d54e46dbf2b886431
kmmerc18 <integerninja@gmail.com>
2020-01-29 15:58:42 -0500

    binary additions and such

---
+++ binary.c
@@ -0,0 +1,51 @@
+# include <stdio.h>
+# define BITS_IN_INT 32
+
+// compute the sum of the digits of a decimal number
+// 1234 = 10
+int sum_of_decimal_digits(int n) {
+	int sum = 0;
+	while (n > 0) {
+		sum = sum + n % 10;
+		n = n / 10
+	}
+	return sum;
+}
+
+int sum_of_bits (int n) {
+	int sum = 0;
+	while (n > 0) {
+		sum = sum + (n & 0b1);
+		n = n >> 1;
+	}
+	return sum;
+}
+
+void print_bin(int n) {
+	if (n == 0) {} else {
+		print_bin(n >> 1);
+		printf("%d", n & 0b1);	// is a recursive boi
+	}
+}
+
+void print_binary(int n) {
+	const int bits_in_int = 32;
+	char bits[bits_in_int];
+	int i = 0;
+	
+	while (n > 0) {
+		bits[i++] = n & 0b1;
+		n = n >> 1;		// >> is shift right (divide by 2)
+	}
+	
+	for (int j = i - 1; j >= 0; j--) {
+	 	printf("%d", bits[j]);
+	}
+	printf("\n");
+}
+
+int main() {
+	print_bin(0b110111101);
+	printf("\n");
+	return 0;
+}
